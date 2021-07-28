29a53d04bb5f236c1111158ebabfd6361509c8ea
czm@local.lan <915734072@qq.com>
2021-07-28 17:18:31 +0800

    最简单的自动dump脚本
    测试完成

--- 镜像库脚本/21.py
+++ 镜像库脚本/21.py
@@ -2,11 +2,28 @@
 #coding:utf-8
 
 import os
-dbname = os.popen("mysql -uroot -p'I.lw@2020' -h 10.20.3.44 -P 3306 -e \"show databases;\"| grep -Evi \"database|infor|perfor\"").read().strip('\n')
-print(dbname)
 
+#获取数据库名称字符串
+dbname = os.popen("mysql -uroot -p'I.lw@2020' -h 10.20.3.44 -P 3306 -e \"show databases;\"| grep -Evi \"database|^mysql$|^sys$|^information_schema$|^performance_schema$\"").read().strip('\n')
+#print(dbname)
+
+#字符串转为列表
 dbnamelist = list(dbname.split('\n'))
-print(dbnamelist)
+#print(dbnamelist)
 
+#在镜像库上新建数据库
 for line in dbnamelist:
-    print(line) 
+    #print(type(line))
+    os.system("mysql -uroot -p'I.lw@2020' -e \"drop database if exists %s;\"" % (line))
+    os.system("mysql -uroot -p'I.lw@2020' -e \"create database %s DEFAULT CHARACTER SET utf8;\"" % (line))
+os.system("mysql -uroot -p'I.lw@2020' -e \"show databases;\"")
+
+#在镜像库上获取源库的dump数据
+for line in dbnamelist:
+    #print(type(line))
+    os.system("mysqldump -uroot -p'I.lw@2020' -h 10.20.3.44 -P 3306 --events -B %s > /mnt/%s.sql" % (line,line))
+
+#在镜像库上还原dump数据
+for line in dbnamelist:
+    os.system("mysql -uroot -p'I.lw@2020' %s < /mnt/%s.sql" % (line,line))
+
